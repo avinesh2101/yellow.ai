363e22da033968f8b97d5ed23e9fafc0d2f096a5
Avinesh Pandey <65820597+avinesh2101@users.noreply.github.com>
2022-02-06 17:42:16 +0530

    Form component added with support of Google sheet
    embedded form is added with ui of chat bot and data get stored in google sheet.

---
+++ src/components/Button/Form/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export const Form = ({ onSubmit }) => {
+  return (
+    // <form onSubmit={onSubmit}>
+    //   <div className="form-group">
+    //     <label htmlFor="name">Name</label>
+    //     <input className="form-control" id="name" />
+    //   </div>
+    //   <div className="form-group">
+    //     <label htmlFor="email">Email address</label>
+    //     <input
+    //       type="email"
+    //       className="form-control"
+    //       id="email"
+    //       placeholder="name@example.com"
+    //     />
+    //   </div>
+    //   <div className="form-group">
+    //     <label htmlFor="number">Phone Number</label>
+    //     <input className="form-control" id="name" />
+    //   </div>
+    //   <div className="form-group">
+    //     <button className="form-control btn btn-primary" type="submit">
+    //       Submit
+    //     </button>
+    //   </div>
+    // </form>
+    // <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSchzFbGaKmLvlXhwnweOksy2fmrZTEZVilCJx2APe3LSdBI8Q/viewform?embedded=true" width="100%" height="763" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    // <script async type="text/javascript" src="https://collectcdn.com/embed.js"></script>
+<iframe src="https://links.collect.chat/61ffae7fbe19f1762af04eed"  width="100%" height="400" frameBorder="0"></iframe>
+  
+    );
+};
+export default Form;
